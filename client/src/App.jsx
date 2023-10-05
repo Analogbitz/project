92d0b7aa70a8e8da8adba694f8ee0b249b1ed7d0
Analogbitz <ballnumb001z@gmail.com>
2023-10-05 18:18:35 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,39 @@
+import SideBar from "./Layout/SideBar";
+import HeaderBar from "./Layout/Headerbar";
+
+import { Routes, Route } from "react-router-dom";
+import { CssBaseline, Box } from "@mui/material";
+import MainPage from "./pages/main";
+import ManagePage from "./pages/manage"
+import MechanicPage from "./pages/Mechanic"
+import CarPage from "./pages/Cars"
+import UserPage from "./pages/Users"
+function App() {
+
+  return (
+    <>
+      <CssBaseline />
+      <div className="app">
+        <SideBar />
+        <main className="content">
+          <HeaderBar />
+          <div className="content-body">
+            <Box m="20px">
+              <Routes>
+                <Route path="/admin" element={<MainPage />} />
+                <Route path="/admin/manage" element={<ManagePage />} />
+                <Route path="/admin/manage/users" element={<UserPage />} />
+                <Route path="/admin/manage/cars" element={<CarPage />} />
+                <Route path="/admin/manage/mechanics" element={<MechanicPage />} />
+              </Routes>
+            </Box>
+
+          </div>
+        </main>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
